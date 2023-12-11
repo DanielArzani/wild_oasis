847ad937a769f71cd5a9fd1c0a82bda218d85e1b
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-12-11 15:56:54 -0500

    initial commit

---
+++ src/utils/mediaQueries.ts
@@ -0,0 +1,41 @@
+/**
+ * How to use
+ * import styled from 'styled-components';
+ * import media from '../utils/mediaQueries';
+ *
+ * const Box = styled.div`
+ * background: blue;
+ *
+ *  @media ${media.md} {
+ *    background: red;
+ *  }
+ * `;
+ * */
+
+type MediaQuerySizes = {
+	xsm: number;
+	sm: number;
+	md: number;
+	l: number;
+	xl: number;
+};
+
+type MediaQueries = {
+	[key in keyof MediaQuerySizes]: string;
+};
+
+const sizes: MediaQuerySizes = {
+	xsm: 320,
+	sm: 640,
+	md: 768,
+	l: 1024,
+	xl: 1280,
+};
+
+const media: MediaQueries = Object.keys(sizes).reduce((acc, label) => {
+	const key = label as keyof MediaQuerySizes;
+	acc[key] = `(min-width: ${sizes[key]}px)`;
+	return acc;
+}, {} as MediaQueries);
+
+export default media;
